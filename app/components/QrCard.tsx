b6f7e55800adea24df028d223e49f1ddf316eb45
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2025-11-14 20:18:22 +0300

    feat: add qr code card

---
+++ app/components/QrCard.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React from "react";
+import { outfit } from "../layout";
+
+type Props = {};
+
+export default function QrCard({}: Props) {
+  return (
+    <article className="h-auto flex flex-col flex-wrap gap-6 pt-4 px-4 pb-10 bg-white rounded-3xl max-w-sm">
+      <figure>
+        <Image
+          src="/images/image-qr-code.png"
+          width={288}
+          height={288}
+          alt="qr code image"
+          className="rounded-3xl"
+        />
+      </figure>
+
+      <div
+        className={`${outfit.className} flex flex-col text-center gap-4 px-4 w-2xs`}
+      >
+        <h1 className="tracking-normall text-xl leading-[120%] text-slate-900 font-black">
+          Improve your front-end skills by building projects
+        </h1>
+        <p className="text-[15px] text-slate-500 leading-[140%] tracking-[0.2px]">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </article>
+  );
+}
